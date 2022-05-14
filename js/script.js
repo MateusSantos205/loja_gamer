89c35b504d0f78e563eaaa0daf8946f45f850ff0
TheDoctor205 <101677207+TheDoctor205@users.noreply.github.com>
2022-05-13 21:18:13 -0300

    css responsivo finalizado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+// Inicia o carosel
+
 const myCarousel = new Carousel(document.querySelector("#myCarousel"), {
     // Your options go here
   });
+
+// Função que oculta o menu
+
+const exibeMenu = () => {
+  // muda a visualização dos itens com a classe .oculta-navba, mudando de hide para show
+  $('.oculta-navbar').toggle()
+}
